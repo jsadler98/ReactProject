69d81a0d649d4895000780291fb5d04407201f35
jsadler98 <joshy.sadler@btinternet.com>
2018-01-28 16:11:54 +0000

    test

--- config/environment.js
+++
@@ -1,5 +0,0 @@
-const port  = process.env.PORT || 4000;
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/ReactProject';
-const secret = 'f^dh@CVis--[P';
-
-module.exports = { port, dbURI, secret };
